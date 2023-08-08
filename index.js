ab8e05f0fe1b07ec87d28a18f01fa5b3266fa8c5
Інга Юрченко <missingusya@hotmail.com>
2023-08-08 14:20:30 +0300

    JS Tasks 1-5

---
+++ index.js
@@ -0,0 +1,43 @@
+//Task  № 1
+
+// const productName = "Droid";
+// const pricePerItem = 2000;
+
+// console.log(productName);
+// console.log(pricePerItem);
+
+// Task № 2
+
+// let pricePerItem = 2000;
+// let productName = "Droid";
+
+// pricePerItem = 3500;
+// productName = "Repair Droid"
+
+// console.log(productName);
+// console.log(pricePerItem);
+
+// Task № 3
+
+// const TopSpeed = 160
+// const distance = 617.54
+// const login = "mango935"
+// const isOnline = false
+// const admin = false
+
+// console.log(admin)
+
+// Task № 4
+
+// const pricePerItem = 3500
+// const orderedQuantity = 4
+// const totalPrice = pricePerItem * orderedQuantity
+// console.log(totalPrice)
+
+// Task № 5
+
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message)  
+
